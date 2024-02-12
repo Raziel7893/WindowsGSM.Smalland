f888e5f64b4f3aefcbf7541d1edc7b02b00bbf8b
Alex <alex@abarthel.me>
2024-02-12 15:47:10 +0100

    Initial version of Smalland Plugin

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# WindowsGSM.Smallands
-Plugin for WindowsGSM to run a dedicated server for Smalland: Survive the Wilds 
+# WindowsGSM.Smalland
+🧩WindowsGSM plugin that provides Smalland Dedicated server
+
+## PLEASE ⭐STAR⭐ THE REPO IF YOU LIKE IT! THANKS!
+
+### WindowsGSM Installation: 
+1. Download  WindowsGSM https://windowsgsm.com/ 
+2. Create a Folder at a Location you wan't all Server to be Installed and Run.
+4. Drag WindowsGSM.Exe into previously created folder and execute it.
+
+### Plugin Installation:
+1. Download [latest](https://https://github.com/Raziel7893/WindowsGSM.Smalland/releases/latest) release
+2. Extract then Move the folder **Smalland.cs** folder to **plugins** folder
+3. OR Press on the Puzzle Icon in the left bottom side and install this plugin by navigating to it and select the Zip File.
+4. Click **[RELOAD PLUGINS]** button or restart WindowsGSM
+5. Navigate "Servers" and Click "Install Game Server" and find "Smalland Dedicated Server [Smalland.cs]
+
+### Official Documentation
+🗃️ Didn't find any documentation yet. Please Let me know if you came accros one
+
+### The Game
+🕹️ https://store.steampowered.com/app/768200/Smalland_Survive_the_Wilds/
+
+### Dedicated server info
+🖥️ https://steamdb.info/app/808040/info/
+
+### Port Forwarding
+- 7777 UDP - Default
+- 7777 TCP - if you are using 8211 it automatically using 8211 +1 = 8212 for QueryPort so you have to port forward this
+
+### Available Params
+All these params are automatically set by WGSM
+- -port=8211                    can be change and working (Change via WGSM settings)
+- -servername=""                can override via Server Start Param box (WGSM Edit button)
+- -serverdescription=""         can override via Server Start Param box (WGSM Edit button)
+- #-NOSTEAM                      Suggested StartParameters from the default start script, not sure what it does, changes nothing, so it is skipped by default. Maybe there is an update on release for it (cann be added in Additional Parameters)
+- -log                          creates logfiles in serverId\serverfiles\SMALLAND\Saved\Logs
+
+### Config Guide
+I Created A configFile to set world-parameters(Including Password) to not clutter the AdditionalParameter Box and dueto strange format of the Start Parameters
+Go to serverId\serverfiles\smalland_conf.json and edit it with your preffered text Editior. 
+- "Password": "",                       Locks Server behind a password promt
+- "FriendlyFire": false,                Friendly fire between players
+- "PeacefullMode": false,               
+- "KeepInventory": false,               Keep inventory on death
+- "NoDeterioration": false,             Disable building wather deterioration
+- "Private": false,                     Private server is hidden on the server browser (Not possible yet, as there is no way to connect without the ServerBrowser)
+- "LengthOfDaySeconds": 1800,           Length of day in seconds (default is 1800 which is 30 minutess)
+- "LengthOfSeasonSeconds": 10800,       Length of season in seconds (default is 10800 which is 3 hours)
+- "CreatureHealthModifier": 100,        Creature health modifier (20-300 default is 100)
+- "CreatureDamageModifier": 100,        Creature damage modifier (20-300 default is 100)
+- "NourishmentLossModifier": 100,       Nourishment loss modifier (0-100 default is 100)
+- "FalldamageModifier": 100,            Fall damage modifier (50-100 default is 100)
+
+The last 4 Options are copied from the shipped Start-Script(serverfiles/start-server.bat) on install. They are apperantly mandatory, so the should not be changed
+- "DeploymentId": "50f2b148496e4cbbbdeefbecc2ccd6a3",
+- "ClientId": "xyza78918KT08TkA6emolUay8yhvAAy2",
+- "ClientSecret": "aN2GtVw7aHb6hx66HwohNM+qktFaO3vtrLSbGdTzZWk",
+- "PrivateKey": ""
+
+### Connecting on a locked server (with password)
+For now only Connecting via a the ingame serverbrowser is possible. It is recommended to note down the 5 Options after password and filter your servers accordingly (as there is no name filter either)
+- On the left of the join window be sure to click public (not official)
+- On the lower part choose your options to filter down the list of servers
+- Be sure to favour your server, so they should show up under Favorites on the next time
+
+### Other notes
+- The game is currently in Early Access Stage WGSM and this plugin is not taking liability if something happens to your server, the app is only for managing your server easily
+
+### How can you play with your friends without port forwarding?
+- Use [zerotier](https://www.zerotier.com/) folow the basic guide and create network
+- Download the client app and join to your network
+- Create static IP address for your host machine
+- Edit WGSM IP Address to your recently created static IP address
+- Give your network ID to your friends
+- After they've joined to your network
+- They can connect using the IP you've created eg: 10.123.17.1:7777
+- Enjoy
+
+### Support
+[WGSM](https://discord.com/channels/590590698907107340/645730252672335893)
+
+### License
+This project is licensed under the MIT License - see the <a href="https://github.com/ohmcodes/WindowsGSM.Palworld/blob/main/LICENSE">LICENSE.md</a> file for details
+
+### Thanks
+Thanks to ohmcodes for the Enshrouded and Palworld Plugins which i used for guidance to create this one
---
+++ Smalland.cs/Smalland.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Query;
+using WindowsGSM.GameServer.Engine;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace WindowsGSM.Plugins
+{
+    public class Smalland : SteamCMDAgent
+    {
+        // - Plugin Details
+        public Plugin Plugin = new Plugin
+        {
+            name = "WindowsGSM.Smalland", // WindowsGSM.XXXX
+            author = "abarthel",
+            description = "WindowsGSM plugin for supporting Smalland Dedicated Server",
+            version = "1.0.0",
+            // url = "https://github.com/ohmcodes/WindowsGSM.Smalland", // Github repository link (Best practice) TODO
+            color = "#34FFeb" // Color Hex
+        };
+
+        // - Settings properties for SteamCMD installer
+        public override bool loginAnonymous => true;
+        public override string AppId => "808040"; // Game server appId Steam
+
+        // - Standard Constructor and properties
+        public Smalland(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        private readonly ServerConfig _serverData;
+        public string Error, Notice;
+
+
+        // - Game server Fixed variables
+        //public override string StartPath => "SMALLANDServer.exe"; // Game server start path
+        public override string StartPath => "SMALLAND\\Binaries\\Win64\\SMALLANDServer-Win64-Shipping.exe";
+        public string FullName = "Smalland Dedicated Server"; // Game server FullName
+        public string ConfigFile = "smalland_conf.json"; // Game server FullName
+        public string StartScript = "start-server.bat";
+        public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
+        public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
+
+        // - Game server default values
+        public string Port = "7777"; // Default port
+
+        public string Additional = "-log"; // Additional server start parameter
+
+        // TODO: Following options are not supported yet, as ther is no documentation of available options
+        public string Maxplayers = "16"; // Default maxplayers        
+        public string QueryPort = "7778"; // Default query port. This is the port specified in the Server Manager in the client UI to establish a server connection.
+        // TODO: Unsupported option
+        public string Defaultmap = "Dedicated"; // Default map name
+        // TODO: Undisclosed method
+        public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
+
+
+        public class SmallandConfig
+        {
+            public string Password = "";
+            public bool FriendlyFire = false;
+            public bool PeacefullMode = false;
+            public bool KeepInventory = false;
+            public bool NoDeterioration= false;
+            public bool Private = false;
+            public int LengthOfDaySeconds = 1800;
+            public int LengthOfSeasonSeconds = 10800;
+            public int CreatureHealthModifier = 100;
+            public int CreatureDamageModifier = 100;
+            public int NourishmentLossModifier = 100;
+            public int FalldamageModifier = 100;
+
+
+            public string DeploymentId= "";
+            public string ClientId = "";
+            public string ClientSecret = "";
+            public string PrivateKey = "";
+        }
+
+        // - Create a default cfg for the game server after installation
+        public async void CreateServerCFG()
+        {
+            //will be passed as start argument from file
+            var config = new SmallandConfig()
+            {
+                Password = "",
+                FriendlyFire = false,
+                PeacefullMode = false,
+                KeepInventory = false,
+                NoDeterioration = false,
+                Private = false,
+                LengthOfDaySeconds = 1800,
+                LengthOfSeasonSeconds = 10800,
+                CreatureHealthModifier = 100,
+                CreatureDamageModifier = 100,
+                NourishmentLossModifier = 100,
+                FalldamageModifier = 100
+            };
+            ReadEngineKeys(config);
+            // Convert the object to JSON format
+            string jsonContent = JsonConvert.SerializeObject(config, Formatting.Indented);
+
+            // Specify the file path
+            string filePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, ConfigFile);
+
+            // Write the JSON content to the file
+            File.WriteAllText(filePath, jsonContent);
+        }
+
+        private void ReadEngineKeys(SmallandConfig config)
+        {
+            string shipScript = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartScript);
+            if (!File.Exists(shipScript))
+            {
+                Error = "shipped start-server.bat not found, cant read Engine:[EpicOnlineServices] values, trying without";
+            }
+            StreamReader sr = new StreamReader(shipScript);
+            var line = sr.ReadLine();
+            while (line != null)
+            {
+                if (line.Contains("set DEPLOYMENTID"))
+                    config.DeploymentId = line.Split('=')[1];
+                if (line.Contains("set CLIENTID"))
+                    config.ClientId = line.Split('=')[1];
+                if (line.Contains("set CLIENTSECRET"))
+                    config.ClientSecret = line.Split('=')[1];
+                if (line.Contains("set PRIVATEKEY"))
+                    config.PrivateKey = line.Split('=')[1];
+
+                line = sr.ReadLine();
+            }
+        }
+
+        // - Start server function, return its Process to WindowsGSM
+        public async Task<Process> Start()
+        {
+            string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
+            if (!File.Exists(shipExePath))
+            {
+                Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
+                return null;
+            }
+
+            string configPath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, ConfigFile);
+            if (!File.Exists(configPath))
+            {
+                Error = $"{configPath} not found";
+                return null;
+            }
+
+            StreamReader configReader = new StreamReader(configPath);
+            var config = JsonConvert.DeserializeObject<SmallandConfig>(configReader.ReadToEnd());
+            //Try gather a password from the gui
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"/Game/Maps/WorldGame/WorldGame_Smalland?SERVERNAME=\"{_serverData.ServerName}\"?WORLDNAME=\"{_serverData.ServerMap}\"");
+            if (!string.IsNullOrWhiteSpace(config.Password)) sb.Append($"?PASSWORD=\"{config.Password}\"");
+            if (config.FriendlyFire) sb.Append("?FRIENDLYFIRE");
+            if (config.PeacefullMode) sb.Append("?PEACEFULMODE");
+            if (config.KeepInventory) sb.Append("?KEEPINVENTORY");
+            if (config.NoDeterioration) sb.Append("?NODETERIORATION");
+            if (config.Private) sb.Append("?PRIVATE");
+            sb.Append($"?lengthofdayseconds={config.LengthOfDaySeconds}");
+            sb.Append($"?lengthofseasonseconds={config.LengthOfSeasonSeconds}");
+            sb.Append($"?creaturehealthmodifier={config.CreatureHealthModifier}");
+            sb.Append($"?creaturedamagemodifier={config.CreatureDamageModifier}");
+            sb.Append($"?nourishmentlossmodifier={config.NourishmentLossModifier}");
+            sb.Append($"?falldamagemodifier={config.FalldamageModifier}");
+            
+            if(!string.IsNullOrEmpty(config.DeploymentId) || !string.IsNullOrEmpty(config.ClientId) || !string.IsNullOrEmpty(config.ClientSecret))
+            {
+                ReadEngineKeys(config);
+            }
+
+            sb.Append($" -ini:Engine:[EpicOnlineServices]:DeploymentId={config.DeploymentId}");
+            sb.Append($" -ini:Engine:[EpicOnlineServices]:DedicatedServerClientId={config.ClientId}");
+            sb.Append($" -ini:Engine:[EpicOnlineServices]:DedicatedServerClientSecret={config.ClientSecret}");
+            if (!string.IsNullOrWhiteSpace(config.PrivateKey)) sb.Append($" -ini:Engine:[EpicOnlineServices]:DedicatedServerPrivateKey={config.PrivateKey}");
+
+            sb.Append($" -port={_serverData.ServerPort} {Additional}");
+
+            // Prepare Process
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    CreateNoWindow = true,  //wird komplett ignoriert?!?
+                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    FileName = shipExePath,
+                    Arguments = sb.ToString(),
+                    WindowStyle = ProcessWindowStyle.Hidden,  //wird komplett ignoriert?!?
+                    UseShellExecute = false
+                },
+                EnableRaisingEvents = true
+            };
+
+            // Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
+            if (AllowsEmbedConsole)
+            {
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                var serverConsole = new ServerConsole(_serverData.ServerID);
+                p.OutputDataReceived += serverConsole.AddOutput;
+                p.ErrorDataReceived += serverConsole.AddOutput;
+            }
+
+            // Start Process
+            try
+            {
+                p.Start();
+                if (AllowsEmbedConsole)
+                {
+                    p.BeginOutputReadLine();
+                    p.BeginErrorReadLine();
+                }
+                return p;
+            }
+            catch (Exception e)
+            {
+                Error = e.Message;
+                return null; // return null if fail to start
+            }
+        }
+
+        // - Stop server function
+        public async Task Stop(Process p)
+        {
+            await Task.Run(() =>
+            {
+                Functions.ServerConsole.SetMainWindow(p.MainWindowHandle);
+                Functions.ServerConsole.SendWaitToMainWindow("^c");
+                p.WaitForExit(2000);
+            });
+        }
+        public async Task<Process> Install()
+        {
+            var steamCMD = new Installer.SteamCMD();
+            Process p = await steamCMD.Install(_serverData.ServerID, string.Empty, AppId, true, loginAnonymous);
+            Error = steamCMD.Error;
+            return p;
+        }
+        public async Task<Process> Update(bool validate = false, string custom = null)
+        {
+            var (p, error) = await Installer.SteamCMD.UpdateEx(serverData.ServerID, AppId, validate, custom: custom, loginAnonymous: loginAnonymous);
+            Error = error;
+            await Task.Run(() => { p.WaitForExit(); });
+
+            return p;
+        }
+
+        public bool IsInstallValid()
+        {
+            return File.Exists(ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath));
+        }
+        public bool IsImportValid(string path)
+        {
+            string importPath = Path.Combine(path, StartPath);
+            Error = $"Invalid Path! Fail to find {Path.GetFileName(StartPath)}";
+            return File.Exists(importPath);
+        }
+        public string GetLocalBuild()
+        {
+            var steamCMD = new Installer.SteamCMD();
+            return steamCMD.GetLocalBuild(_serverData.ServerID, AppId);
+        }
+        public async Task<string> GetRemoteBuild()
+        {
+            var steamCMD = new Installer.SteamCMD();
+            return await steamCMD.GetRemoteBuild(AppId);
+        }
+    }
+}
